3669dbef4df32e1a0cfbf9696c9111e6b6a3fee8
Izzatulloh <Izzatulloh@gmail.com>
2025-02-17 14:48:07 +0500

    generic-filter

--- src/Components/Lids/All/style.js
+++ src/Components/Lids/All/style.js
@@ -1,3 +1,18 @@
 import styled from "styled-components";
+import edit from "../../../assets/icons/edit.svg?react";
 
-export const Container= styled.div``
+export const Container = styled.div``
+export const Action = styled.div`
+display: flex;
+justify-content: flex-end;
+`
+
+Action.Edit = styled(edit)`
+padding: 10px;
+width: 42px;
+height: 42px;
+`;
+Action.Move = styled(edit)`
+padding: 10px;
+width: 42px;
+height: 42px;`;
